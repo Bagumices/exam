0fb9cd1822b3ee5c33c130339fc7f6398778e89a
Babushkin Roman <bagumices@gmail.com>
2019-06-10 00:15:53 +0500

    add trash

---
+++ forecasting/trash.py
@@ -0,0 +1,10 @@
+# acc - 0.1
+# model = Sequential()
+# model.add(Dense(32, activation='relu', input_dim=2))
+# model.add(Dense(3, activation='softmax'))
+# model.compile(optimizer='rmsprop',
+#               loss='categorical_crossentropy',
+#               metrics=['accuracy'])
+
+
+
